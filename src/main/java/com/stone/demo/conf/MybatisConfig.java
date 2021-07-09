6ca3a3dce2a25093bccb7370423f3246f4927659
AlexWoo <1152936469@qq.com>
2021-07-09 21:17:50 +0800

    更新mp配置

---
+++ src/main/java/com/stone/demo/conf/MybatisConfig.java
@@ -0,0 +1,31 @@
+package com.stone.demo.conf;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author NJUPT wly
+ * @Date 2021/7/8 9:55 下午
+ * @Version 1.0
+ */
+@Configuration
+public class MybatisConfig {
+
+    /**
+     * 新的分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        //乐观锁插件
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+    }
+
+}
